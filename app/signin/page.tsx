3f921219e50a96cb8c84193912d90a18a8fd9063
syedanasquadri <syedanasquadri123@gmail.com>
2026-02-06 11:37:30 +0530

    added signup signin buttons

---
+++ app/signin/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+export default function Signin(){
+    return <div className="w-screen h-screen flex items-center justify-center">
+        <div className="border p-2">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button onClick={() => {
+
+        }}>Sign in</button>
+        </div>
+
+    </div>
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+export default function Signup(){
+    return <div className="w-screen h-screen flex items-center justify-center">
+        <div className="border p-2">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button onClick={() => {
+
+        }}>Sign up</button>
+        </div>
+
+    </div>
+}
