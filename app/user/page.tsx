0aa46964c26e2fbbfb07954e6ac9ff240a30e60d
syedanasquadri <syedanasquadri123@gmail.com>
2026-02-06 10:57:55 +0530

    updated user page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export default async function User() {
-    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+    const response = await axios.get("http://localhost:3000/api/v1/user/details");
 
     await new Promise(r => setTimeout(r,5000))
 
     const data = response.data;
 
     return <div>
-        user page
         {data.name}
+        {data.email}
     </div>
 }
