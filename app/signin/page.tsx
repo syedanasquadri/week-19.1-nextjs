17db2b94c0cf575906d2bf0a8fd4e445717dec92
syedanasquadri <syedanasquadri123@gmail.com>
2026-02-08 23:27:31 +0530

    updated signup signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,13 +1,18 @@
 'use client'
+
+import axios from "axios"
+
 export default function Signin(){
     return <div className="w-screen h-screen flex items-center justify-center">
         <div className="border p-2">
         <input type="text" placeholder="username"/>
         <input type="password" placeholder="password"/>
         <button onClick={() => {
-
+            axios.post("http://localhost:3000/api/v1/signin",{
+                username,
+                password
+            })
         }}>Sign in</button>
         </div>
-
     </div>
 }
